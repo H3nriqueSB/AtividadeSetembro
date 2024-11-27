f3947e7ae4b0ab6898d91f0aaa4609d670820371
H3nriqueSB <henrique49880516@aluno.ms.senac.br>
2024-11-27 12:02:30 -0300

    Add files via upload

---
+++ HTML/ATV2711/atv2711.js
@@ -0,0 +1,9 @@
+// function validar(){
+//     const x = document.getElementById('inputX').value;
+//     if ((/[az]/i.test(x))) {
+//         document.write('A')
+//     } else {
+//         document.write('B')
+//     }
+// }
+
